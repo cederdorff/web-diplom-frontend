61ea167ac4c456eb57d666958b00ebbe5819ea18
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-10-05 08:48:36 +0200

    project templates added to repo

---
+++ project-template/app.js
@@ -0,0 +1,13 @@
+// declaring a variable with a value
+let message = "Hi Students";
+
+// accessing the variable and logging it to the console
+console.log(message);
+
+// appending the value from the variable to thee DOM element #content
+document.querySelector("#content").innerHTML = message;
+
+// showing and alert with a message
+function showAlert() {
+    alert("Open your Developer Console!");
+}
