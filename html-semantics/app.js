4604d7461a997828c19c3ea87818ce0f1adb9792
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-10-10 11:57:57 +0200

    Update app.js

--- html-semantics/app.js
+++ html-semantics/app.js
@@ -1,8 +1,1 @@
-// declaring a variable with a value
-let message = "Hi Students";
-
-// accessing the variable and logging it to the console
-console.log(message);
-
-// appending the value from the variable to thee DOM element #content
-document.querySelector("#content").innerHTML = message;
+console.log("app.js ready 🎉");
