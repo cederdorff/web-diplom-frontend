8435c14a645e4d72d188a6c86a8a5c65a853f66d
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-10-10 22:21:36 +0200

    normalize css added

---
+++ project-template-normalize-css/app.js
@@ -0,0 +1,8 @@
+// declaring a variable with a value
+let message = "Hi Students";
+
+// accessing the variable and logging it to the console
+console.log(message);
+
+// appending the value from the variable to thee DOM element #content
+document.querySelector("#content").innerHTML = message;
