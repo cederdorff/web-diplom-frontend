dc716761245677665b9e772e8ae656151441b8b7
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-10-30 21:08:53 +0100

    modules-array-persons added

---
+++ modules-array-persons/data.js
@@ -0,0 +1,32 @@
+export const persons = [
+    {
+        name: "Birgitte Kirk Iversen",
+        mail: "bki@mail.dk",
+        title: "Senior Lecturer",
+        img: "https://www.eaaa.dk/media/u4gorzsd/birgitte-kirk-iversen2.jpg?width=800&height=450"
+    },
+    {
+        name: "Martin Aagaard Nøhr",
+        mail: "mnor@mail.dk",
+        title: "Lecturer",
+        img: "https://www.eaaa.dk/media/oayjq02h/martin-n%C3%B8hr.jpg?width=800&height=450"
+    },
+    {
+        name: "Rasmus Cederdorff",
+        mail: "race@mail.dk",
+        title: "Senior Lecturer",
+        img: "https://www.eaaa.dk/media/devlvvgj/rasmus-cederdorff.jpg?width=800&height=450"
+    },
+    {
+        name: "Dan Okkels Brendstrup",
+        mail: "dob@mail.dk",
+        title: "Lecturer",
+        img: "https://www.eaaa.dk/media/bdojel41/dan-okkels-brendstrup.jpg?width=800&height=450"
+    },
+    {
+        name: "Anne Kirketerp",
+        mail: "anki@mail.dk",
+        title: "Head of Department",
+        img: "https://www.baaa.dk/media/5buh1xeo/anne-kirketerp.jpg?width=800&height=450"
+    }
+];
