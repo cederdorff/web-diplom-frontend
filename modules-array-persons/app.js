614a0ee88e913d9076af7a8bd4762977bdaed6a6
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-10-30 21:17:45 +0100

    Update app.js

--- modules-array-persons/app.js
+++ modules-array-persons/app.js
@@ -1,8 +1,9 @@
 import { persons } from "./data.js";
+import { search, sortPersons, showLecturers } from "./helpers.js";
 
 console.log(persons);
 
-function displayPersons(listOfPersons) {
+export function displayPersons(listOfPersons) {
     let html = "";
     //loop through all persons and create an article with content for each
     for (const person of listOfPersons) {
@@ -36,52 +37,7 @@ function savePerson(event) {
     displayPersons(persons); // make sure to reload all persons
     form.reset(); // reset (clear) input fields
 }
-
-function search(event) {
-    const searchValue = event.target.value.toLowerCase();
-
-    const results = [];
-
-    for (const person of persons) {
-        const name = person.name.toLowerCase();
-        if (name.includes(searchValue)) {
-            results.push(person);
-        }
-    }
-    displayPersons(results);
-}
-
-// function search(event) {
-//     const searchValue = event.target.value.toLowerCase();
-//     const results = persons.filter(person => person.name.toLowerCase().includes(searchValue));
-//     displayPersons(results);
-// }
-
-function sortPersons(event) {
-    const option = event.target.value;
-    if (option === "name") {
-        console.log("sort by name");
-        persons.sort((person1, person2) => person1.name.localeCompare(person2.name));
-    } else if (option === "title") {
-        console.log("sort by title");
-        persons.sort((person1, person2) => person1.title.localeCompare(person2.title));
-    }
-    displayPersons(persons);
-}
-
-function showLecturers(event) {
-    const showLecturers = event.target.checked;
-    console.log(showLecturers);
-
-    if (showLecturers) {
-        displayPersons(persons);
-    } else {
-        const results = persons.filter(person => person.title === "Senior Lecturer");
-        displayPersons(results);
-    }
-}
-
-// ====== Event Listeners ====== //
+// ========== event listeners ========== //
 document.querySelector("#save-form").addEventListener("submit", savePerson);
 document.querySelector("#search").addEventListener("keyup", search);
 document.querySelector("#search").addEventListener("search", search); // clear button
