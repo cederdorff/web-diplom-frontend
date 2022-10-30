04e9739b054ada5fa5d46128ece247472e9933b8
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-10-30 21:00:33 +0100

    sort-filter-search-array-persons added

---
+++ sort-filter-search-array-persons/app.js
@@ -0,0 +1,114 @@
+const persons = [
+    {
+        name: "Birgitte Kirk Iversen",
+        mail: "bki@mail.dk",
+        title: "Senior Lecturer",
+        img: "https://www.eaaa.dk/media/u4gorzsd/birgitte-kirk-iversen2.jpg?width=800&height=450"
+    },
+    {
+        name: "Martin Aagaard Nøhr",
+        mail: "mnor@mail.dk",
+        title: "Lecturer",
+        img: "https://www.eaaa.dk/media/oayjq02h/martin-n%C3%B8hr.jpg?width=800&height=450"
+    },
+    {
+        name: "Rasmus Cederdorff",
+        mail: "race@mail.dk",
+        title: "Senior Lecturer",
+        img: "https://www.eaaa.dk/media/devlvvgj/rasmus-cederdorff.jpg?width=800&height=450"
+    },
+    {
+        name: "Dan Okkels Brendstrup",
+        mail: "dob@mail.dk",
+        title: "Lecturer",
+        img: "https://www.eaaa.dk/media/bdojel41/dan-okkels-brendstrup.jpg?width=800&height=450"
+    }
+];
+
+console.log(persons);
+
+function displayPersons(listOfPersons) {
+    let html = "";
+    //loop through all persons and create an article with content for each
+    for (const person of listOfPersons) {
+        html += /*html*/ `
+            <article>
+                <img src="${person.img}">
+                <h2>${person.name}</h2>
+                <p>${person.title}</p>
+                <a href="mailto:${person.mail}">${person.mail}</a>
+            </article>
+        `;
+    }
+    document.querySelector("#content").innerHTML = html; // set grid container with person <article>-s
+}
+
+displayPersons(persons);
+
+function savePerson(event) {
+    event.preventDefault(); // prevent form refreshing page
+    const form = event.target; // save reference to form in varaiable
+
+    // create new person object based on input values
+    const newPerson = {
+        name: form.name.value,
+        mail: form.mail.value,
+        title: form.title.value,
+        img: form.url.value
+    };
+
+    persons.push(newPerson); // add new peron object to array (persons)
+    displayPersons(persons); // make sure to reload all persons
+    form.reset(); // reset (clear) input fields
+}
+
+function search(event) {
+    const searchValue = event.target.value.toLowerCase();
+
+    const results = [];
+
+    for (const person of persons) {
+        const name = person.name.toLowerCase();
+        if (name.includes(searchValue)) {
+            results.push(person);
+        }
+    }
+    displayPersons(results);
+}
+
+// function search(event) {
+//     const searchValue = event.target.value.toLowerCase();
+//     const results = persons.filter(person => person.name.toLowerCase().includes(searchValue));
+//     displayPersons(results);
+// }
+
+function sortPersons(event) {
+    const option = event.target.value;
+    if (option === "name") {
+        console.log("sort by name");
+        persons.sort((person1, person2) => person1.name.localeCompare(person2.name));
+    } else if (option === "title") {
+        console.log("sort by title");
+        persons.sort((person1, person2) => person1.title.localeCompare(person2.title));
+    }
+    displayPersons(persons);
+}
+
+function showLecturers(event) {
+    const showLecturers = event.target.checked;
+    console.log(showLecturers);
+
+    if (showLecturers) {
+        displayPersons(persons);
+    } else {
+        const results = persons.filter(person => person.title === "Senior Lecturer");
+        displayPersons(results);
+    }
+}
+
+// ====== Event Listeners ====== //
+document.querySelector("#save-form").addEventListener("submit", savePerson);
+document.querySelector("#search").addEventListener("keyup", search);
+document.querySelector("#search").addEventListener("search", search); // clear button
+document.querySelector("#sort-by").addEventListener("change", sortPersons);
+document.querySelector("#show-lecturers").addEventListener("change", showLecturers);
