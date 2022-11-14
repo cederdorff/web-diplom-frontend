7e31a7c31a417c21307a556912e247e86722b7b0
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-11-14 18:09:55 +0100

    react-cdn-template

---
+++ react-cdn-template/app.jsx
@@ -0,0 +1,17 @@
+import React from "https://cdn.skypack.dev/react";
+import ReactDOM from "https://cdn.skypack.dev/react-dom";
+
+function Greeting({ name }) {
+    return <h1>Hello, {name}</h1>;
+}
+
+function App() {
+    const name = "Peter";
+    return (
+        <section>
+            <Greeting name="React" />
+        </section>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
