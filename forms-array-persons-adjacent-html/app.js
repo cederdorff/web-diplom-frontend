0ad62a9208f07089c0359d3025b2cf17acf36230
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-11-16 14:53:59 +0100

    forms-array-persons-adjacent-html

---
+++ forms-array-persons-adjacent-html/app.js
@@ -0,0 +1,72 @@
+let persons = [
+    {
+        name: "Birgitte Kirk Iversen",
+        mail: "bki@mail.dk",
+        title: "Senior Lecturer",
+        img: "https://www.eaaa.dk/media/u4gorzsd/birgitte-kirk-iversen2.jpg?width=800&height=450"
+    },
+    {
+        name: "Martin Aagaard Nøhr",
+        mail: "mnor@mail.dk",
+        title: "Lecturer",
+        img: "https://www.eaaa.dk/media/oayjq02h/martin-n%C3%B8hr.jpg?width=800&height=450"
+    },
+    {
+        name: "Rasmus Cederdorff",
+        mail: "race@mail.dk",
+        title: "Senior Lecturer",
+        img: "https://www.eaaa.dk/media/devlvvgj/rasmus-cederdorff.jpg?width=800&height=450"
+    },
+    {
+        name: "Dan Okkels Brendstrup",
+        mail: "dob@mail.dk",
+        title: "Lecturer",
+        img: "https://www.eaaa.dk/media/bdojel41/dan-okkels-brendstrup.jpg?width=800&height=450"
+    }
+];
+
+console.log(persons);
+
+function displayPersons() {
+    document.querySelector("#content").innerHTML = "";
+    //loop through all persons and create an article with content for each
+    for (const person of persons) {
+        document.querySelector("#content").insertAdjacentHTML(
+            "beforeend",
+            /*html*/ `
+            <article>
+                <img src="${person.img}">
+                <h2>${person.name}</h2>
+                <p>${person.title}</p>
+                <a href="mailto:${person.mail}">${person.mail}</a>
+            </article>
+        `
+        );
+        document.querySelector("article:last-child").addEventListener("click", () => deletePerson(person));
+    }
+}
+
+displayPersons();
+
+function deletePerson(person) {
+    console.log(person);
+    persons = persons.filter(item => item != person);
+    displayPersons();
+}
+
+function savePerson(event) {
+    event.preventDefault(); // prevent form refreshing page
+    const form = event.target; // save reference to form in varaiable
+
+    // create new person object based on input values
+    const newPerson = {
+        name: form.name.value,
+        mail: form.mail.value,
+        title: form.title.value,
+        img: form.url.value
+    };
+
+    persons.push(newPerson); // add new peron object to array (persons)
+    displayPersons(); // make sure to reload all persons
+    form.reset(); // reset (clear) input fields
+}
